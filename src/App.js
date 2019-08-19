d69e0f5f40ec01307ce3959c12ff4490503bc781
Steven Cheong <steven.cheong@telus.com>
2019-08-19 01:06:50 -0700

    loop thru todos with map

--- src/App.js
+++ src/App.js
@@ -3,10 +3,34 @@ import './App.css';
 import Todos from './components/Todos'
 
 class App extends Component {
+
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Dinner with wife',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Meeting with boss',
+        completed: false
+      },
+    ]
+  }
+
   render() {
+
+    // console.log(this.state.todos)
+
     return (
       <div className="App">
-          <Todos />
+          <Todos todos={this.state.todos} />
       </div>
     );
   }
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -5,11 +5,18 @@ import React, {Component} from 'react';
 
 class Todos extends Component {
   render() {
-    return (
-      <div>
-          <h1>Todos</h1>
-      </div>
-    );
+
+    console.log(this.props.todos)
+
+    return this.props.todos.map( (todo) => (
+        <h3> {todo.title} </h3>
+    ))
+
+    // return (
+    //   <div>
+    //       <h1> {this.props.todos[1].title} </h1>
+    //   </div>
+    // );
   }
 }
 
