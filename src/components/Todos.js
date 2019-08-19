799e44c1526e232351c4b169fa580703fe27bd42
Steven Cheong <steven.cheong@telus.com>
2019-08-19 02:18:02 -0700

    move markComplete all the way to app.js

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -5,9 +5,11 @@ import PropTypes from 'prop-types'
 
 class Todos extends Component {
   
+
+  
   render() {
     return this.props.todos.map( (todo) => (
-        <TodoItem key={todo.id} todo={todo} markComplete={this.props.markComplete.bind(this, this.props.todo.id)} />
+        <TodoItem key={todo.id} todo={todo} markComplete={this.props.markComplete} />
     ))
   }
 }
