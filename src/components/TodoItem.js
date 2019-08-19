3b0e231fffdd43444f327cd7f851b175e4e64505
Steven Cheong <steven.cheong@telus.com>
2019-08-19 01:43:35 -0700

    style const

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 export class TodoItem extends Component {
     render() {
         return (
-            <div style={{ backgroundColor: '#f4f4f4' }}>
+            <div style={itemStyle}>
                 <p> {this.props.todo.title} </p>
             </div>
         )
@@ -14,6 +14,10 @@ export class TodoItem extends Component {
 // PropTypes
 TodoItem.propTypes = {
     todo: PropTypes.object.isRequired
-  }
+}
+
+const itemStyle = { 
+    backgroundColor: '#f4f4f4'
+}
 
 export default TodoItem
