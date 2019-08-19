2ea9eb2cca5dac1c7addb16172dd66e2b37a4b87
Steven Cheong <steven.cheong@telus.com>
2019-08-19 00:25:05 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello world
+        </p>
+        <p>
+          Edit <code>src/App.js</code> and save to reload 2.
         </p>
         <a
           className="App-link"
