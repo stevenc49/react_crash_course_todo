39c1c1e6284cadaa30e782c7d485a4883cf119e2
Steven Cheong <steven.cheong@telus.com>
2019-08-19 01:42:39 -0700

    style inline

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 export class TodoItem extends Component {
     render() {
         return (
-            <div>
+            <div style={{ backgroundColor: '#f4f4f4' }}>
                 <p> {this.props.todo.title} </p>
             </div>
         )
