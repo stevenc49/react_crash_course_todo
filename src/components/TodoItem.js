ac0024a6dda2863aac288363c9fe785df4d28ed8
Steven Cheong <steven.cheong@telus.com>
2019-08-19 02:01:13 -0700

    onchange

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -11,22 +11,21 @@ export class TodoItem extends Component {
             borderBottom: '1px ccc dotted',
             textDecoration: this.props.todo.completed ? 'line-through' : 'none'
         }
+    }
 
-        // if(this.props.todo.completed) {
-        //     return {
-        //         textDecoration: 'line-through'
-        //     }
-        // } else {
-        //     return {
-        //         textDecoration: 'none'
-        //     }
-        // }
+    markComplete = (e) => {
+
+        console.log(this.props)
+
     }
 
     render() {
         return (
             <div style={this.getStyle()}>
-                <p> {this.props.todo.title} </p>
+                <p>
+                    <input type="checkbox" onChange={this.markComplete} /> {' '}
+                    {this.props.todo.title}
+                </p>
             </div>
         )
     }
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -6,8 +6,6 @@ import PropTypes from 'prop-types'
 class Todos extends Component {
   render() {
 
-    console.log(this.props.todos)
-
     return this.props.todos.map( (todo) => (
 
         <TodoItem key={todo.id} todo={todo} />
