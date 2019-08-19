80b11e173e439bc1ea2a40b3530e80590bd3a883
Steven Cheong <steven.cheong@telus.com>
2019-08-19 01:53:00 -0700

    all css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import './App.css';
 import Todos from './components/Todos'
 
 class App extends Component {
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -6,6 +6,9 @@ export class TodoItem extends Component {
     getStyle = () => {
 
         return {
+            backgroundColor: '#f4f4f4',
+            padding: '10px',
+            borderBottom: '1px ccc dotted',
             textDecoration: this.props.todo.completed ? 'line-through' : 'none'
         }
 
