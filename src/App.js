90ad917b11310bfbf0f26eadf260e75b97d12c3d
Steven Cheong <steven.cheong@telus.com>
2019-08-19 02:54:39 -0700

    time for bed

--- src/App.js
+++ src/App.js
@@ -37,11 +37,7 @@ class App extends Component {
 
 
 delTodo = (id) => {
-
-  console.log( id )
-
   this.setState({ todos: [...this.state.todos.filter((todo) => todo.id !== id)] })
-
 }
 
   render() {
