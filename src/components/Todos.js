98e0d84999e49286f0e43f1f91a6ce6c64b43593
Steven Cheong <steven.cheong@telus.com>
2019-08-19 01:33:50 -0700

    todoitem

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,6 +1,6 @@
 
 import React, {Component} from 'react';
-
+import TodoItem from './TodoItem'
 
 
 class Todos extends Component {
@@ -9,7 +9,9 @@ class Todos extends Component {
     console.log(this.props.todos)
 
     return this.props.todos.map( (todo) => (
-        <h3> {todo.title} </h3>
+
+        <TodoItem key={todo.id} todo={todo} />
+
     ))
 
     // return (
