3332f1a1bc5f88e0fba1e4d28058937e6a8f0b0d
Steven Cheong <steven.cheong@telus.com>
2019-08-20 00:47:27 -0700

    submit form

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react'
 
 export class AddTodo extends Component {
+
+    state = {
+        title: ''
+    }
+
+    // onChange = (e) => this.setState({title: e.target.value});
+    onChange = (e) => this.setState({ [e.target.name] : e.target.value});
+
     render() {
         return (
             <form style={{display:'flex'}}>
@@ -9,6 +17,8 @@ export class AddTodo extends Component {
                     name='title'
                     style={{flex:'10', padding: '5px'}}
                     placeholder='Add Todo...'
+                    value={this.state.value}
+                    onChange={this.onChange}
                 />
 
                 <input 
